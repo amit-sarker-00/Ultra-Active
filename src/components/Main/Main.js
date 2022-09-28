0a18045715e50e3a63f49966fb0f82bca6d77022
Amit Sarker <amitsarker255@gmail.com>
2022-09-28 21:20:27 +0600

    font awesome icon added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Main.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSprout } from "@fortawesome/free-solid-svg-icons";
 const Main = () => {
   return (
     <div className="main-container">
       <div className="container">
-        <h2>FITNESS CLUB</h2>
+        <h2>
+          <FontAwesomeIcon icon={faSprout} /> FITNESS CLUB
+        </h2>
       </div>
       <div className="right-container">Hello i'm from right bar</div>
     </div>
