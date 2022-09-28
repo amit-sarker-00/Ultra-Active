83838bdcc35dcfef86ee0a2496b53548a79f555d
Amit Sarker <amitsarker255@gmail.com>
2022-09-28 21:14:07 +0600

    website name and title added

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Main.css";
+const Main = () => {
+  return (
+    <div className="main-container">
+      <div className="container">
+        <h2>FITNESS CLUB</h2>
+      </div>
+      <div className="right-container">Hello i'm from right bar</div>
+    </div>
+  );
+};
+
+export default Main;
